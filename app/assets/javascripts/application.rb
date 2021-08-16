67b7782090b5012e2abefcfb473d8eb2ad03c222
Andy Maleh <andy.am@gmail.com>
2021-08-16 16:27:33 -0400

    Upgrade to glimmer-dsl-opal 0.20.0 + Hello, Spinner!

--- app/assets/javascripts/application.rb
+++ app/assets/javascripts/application.rb
@@ -201,6 +201,14 @@ shell {
     }
   }
   button {
+    text 'Hello, Spinner!'
+    font height: 20
+    
+    on_widget_selected {
+      require 'glimmer-dsl-opal/samples/hello/hello_spinner'
+    }
+  }
+  button {
     text 'Hello, Tab!'
     font height: 20
     
